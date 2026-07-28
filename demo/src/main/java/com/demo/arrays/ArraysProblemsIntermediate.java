9d1f333c7488ab41454473d3d662e5d5eceaea49
manish wani <manishwani03@gmail.com>
2026-07-28 19:44:05 +0530

    first intermediate

--- demo/src/main/java/com/demo/arrays/ArraysProblemsIntermediate.java
+++ demo/src/main/java/com/demo/arrays/ArraysProblemsIntermediate.java
@@ -2,7 +2,20 @@ package com.demo.arrays;
 
 public class ArraysProblemsIntermediate {
 
-    public static void main(String[] args) {
+    // find missing numbers
+    public static void findMissingNumber(int[] arr) {
+        int n = arr.length + 1;
+        int expected = n * (n + 1) / 2;
+        int actual = 0;
 
+        for (int num : arr) {
+            actual += num;
+        }
+        System.out.println("Missing Number : " + (expected - actual));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 6};
+        findMissingNumber(arr);
     }
 }
