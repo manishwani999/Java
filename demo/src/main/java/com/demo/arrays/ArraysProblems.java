c5422bb2b6a1741a2c75eba776080ff964effaaa
manish wani <manishwani03@gmail.com>
2026-07-28 18:56:33 +0530

    array

--- demo/src/main/java/com/demo/arrays/ArraysProblems.java
+++ demo/src/main/java/com/demo/arrays/ArraysProblems.java
@@ -2,6 +2,7 @@ package com.demo.arrays;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class ArraysProblems {
 
@@ -59,6 +60,24 @@ public class ArraysProblems {
         System.out.println("Second Small Element : " + sSmall);
     }
 
+    // Reverse an Array
+    public static void reverseArray(int[] arr) {
+        System.out.println("Array Before Reverse : " + Arrays.toString(arr));
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println("Array After Reverse :  " + Arrays.toString(arr));
+    }
+
     // check array is sorted or not
     public static void checkSortedArray(int[] arr) {
 
@@ -113,10 +132,79 @@ public class ArraysProblems {
         System.out.println(set);
     }
 
+    // Move all zeros to end
+    public static void moveAllZerosToEnd(int[] arr) {
+        int index = 0; // { 4, 0, 5, 2, 0, 1, 0, 0, 2 };
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // left rotate array by one
+    public static void leftRotateArrayByOne(int[] arr) {
+        int first = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = first;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // right rotate array by one
+    public static void rightRotateArrayByOne(int arr[]) {
+        int last = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[0] = last;
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // right rotate by k
+    public static void rightRotateByK(int[] arr) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter K : ");
+        int k = sc.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            int last = arr[arr.length - 1];
+            for (int j = arr.length - 2; j >= 0; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[0] = last;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // rotate array left by k
+    public static void leftRotateByK(int arr[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter K : ");
+        int k = sc.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            int first = arr[0];
+            for (int j = 1; j < arr.length; j++) {
+                arr[j - 1] = arr[j];
+            }
+            arr[arr.length - 1] = first;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 40, 5 };
+        int[] arr = { 1, 2, 3, 4, 5 };
         int[] sort = { 5, 3, 1, 2, 4 };
-        int[] arrr = { 4 };
+        int[] arrr = { 4, 0, 5, 2, 0, 1, 0, 0, 2 };
         int[] dup = { 1, 2, 2, 3, 4, 5, 5 };
 
         // findLargest(arr);
@@ -124,13 +212,22 @@ public class ArraysProblems {
         // findSmallest(arr);
         // findSecondSmallest(arr);
         // checkSortedArray(arrr);
-        int length = removeDuplicatesSorted(dup);
-        for (int i = 0; i < length; i++) {
-            System.out.print(dup[i] + " ");
-        }
+        /*
+         * int length = removeDuplicatesSorted(dup);
+         * for (int i = 0; i < length; i++) {
+         * System.out.print(dup[i] + " ");
+         * }
+         */
 
-        System.out.println();
-        removeDuplicatesUnsorted(dup);
+        // System.out.println();
+        // removeDuplicatesUnsorted(dup);
+
+        // reverseArray(arr);
+        // moveAllZerosToEnd(arrr);
+        // leftRotateArrayByOne(arr);
+        rightRotateArrayByOne(arr);
+        // rightRotateByK(arr);
+        // leftRotateByK(arr);
 
     }
 }
